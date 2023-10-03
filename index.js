05397e4038b5b19b08a00e68507490d43a3cf89f
Matthew Tran <matt.h.tran@gmail.com>
2023-10-03 17:14:36 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide (num1, num2){
+    return num1/num2;
+}
+
+divide(2000,100);
+
+const square = (parameter1) => parameter1 * parameter1; square(2,2);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2; add()
